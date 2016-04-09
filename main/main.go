156e285f4112591272c67d3fb75f0acde2092fb0
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2016-04-09 17:14:14 +0530

    renamed main file and created new main file with file server.

--- main/main.go
+++ main/main.go
@@ -2,48 +2,9 @@ package main
 
 import (
 	"net/http"
-	"strings"
-	"os"
-	"bufio"
 )
 
 func main() {
-	http.Handle("/", new(MyHandler))
-	http.ListenAndServe(":8070", nil)
+	http.ListenAndServe(":8070", http.FileServer(http.Dir("public")))
 }
 
-type MyHandler struct {
-	http.Handler
-}
-
-func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	path := "public/" + req.URL.Path
-	f, err := os.Open(string(path))
-	if err == nil {
-
-		bufferedReader := bufio.NewReader(f)
-		var contentType string
-
-		if strings.HasSuffix(path, ".css") {
-			contentType = "text/css"
-		} else if strings.HasSuffix(path, ".html") {
-			contentType = "text/html"
-		} else if strings.HasSuffix(path, ".js") {
-			contentType = "application/javascript"
-		} else if strings.HasSuffix(path, ".png") {
-			contentType = "image/png"
-		} else if strings.HasSuffix(path, ".jpg") {
-			contentType = "image/jpg"
-		} else if strings.HasSuffix(path, ".gif") {
-			contentType = "image/gif"
-		}else {
-			contentType = "text/plain"
-		}
-		w.Header().Add("Content-Type", contentType)
-		bufferedReader.WriteTo(w)
-
-	}else {
-		w.WriteHeader(404)
-		w.Write([]byte("404-" + http.StatusText(404)))
-	}
-}
