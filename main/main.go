f0809feb3563fae920ac81b425fb088e2b112150
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2016-04-09 16:18:04 +0530

    Added handler to main go file and added index html file in public folder to serve

--- main/main.go
+++ main/main.go
@@ -1,10 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"io/ioutil"
+)
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte ("Hello World"))
-	})
-	http.ListenAndServe(":8080",nil)
+	http.Handle("/", new (MyHandler))
+	http.ListenAndServe(":8070", nil)
+}
+
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public/" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+	if err == nil {
+		w.Write(data)
+	}else {
+		w.WriteHeader(404)
+		w.Write([]byte("404-" + http.StatusText(404)))
+	}
 }
