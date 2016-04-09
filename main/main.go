c2c0f913869548a2b266b5bc20e64e6d48369b97
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2016-04-09 15:35:25 +0530

    Main go file for web application.

---
+++ main/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte ("Hello World"))
+	})
+	http.ListenAndServe(":8080",nil)
+}
