c43b514fc4f2c2ad6e949aebccd74daa69b31f53
Jalpesh Vadgama <jalpesh@dotnetjalps.com>
2016-04-09 16:49:09 +0530

    Added images file and html for the buffer.

--- main/main.go
+++ main/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
-	"io/ioutil"
+	"strings"
+	"os"
+	"bufio"
 )
 
 func main() {
-	http.Handle("/", new (MyHandler))
+	http.Handle("/", new(MyHandler))
 	http.ListenAndServe(":8070", nil)
 }
 
@@ -16,9 +18,24 @@ type MyHandler struct {
 
 func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	path := "public/" + req.URL.Path
-	data, err := ioutil.ReadFile(string(path))
+	f, err := os.Open(string(path))
 	if err == nil {
-		w.Write(data)
+		bufferedReader := bufio.NewReader(f)
+		var contentType string
+
+		if strings.HasSuffix(path, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "application/javascript"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		}else {
+			contentType = "text/plain"
+		}
+		w.Header().Add("Content Type", contentType)
+		bufferedReader.WriteTo(w)
 	}else {
 		w.WriteHeader(404)
 		w.Write([]byte("404-" + http.StatusText(404)))
